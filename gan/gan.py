d10a1c176da6d2f230dcb3789d604058f8767941
Shivam Pandey <48156087+shivam-pandey-15@users.noreply.github.com>
2020-04-12 16:07:33 +0530

    Update gan.py

--- gan/gan.py
+++ gan/gan.py
@@ -37,56 +37,42 @@ else:
 
 #Loading and Creating Batches of Dataset
 transform  = transforms.Compose([transforms.ToTensor(),transforms.Normalize([0.5],[0.5])])
-
 mnist = datasets.MNIST(root ='../data/mnist_data',download = True,transform= transform)
-
 data  = torch.utils.data.DataLoader(mnist,batch_size = args.batch_size,shuffle = True,drop_last=True)
-
 
 #Discriminator
 class Discriminator(nn.Module):
-
     def __init__(self):
         super(Discriminator,self).__init__()
-
         in_feature = 784
         hid = 256
         out = 1
-
         self.hidden = nn.Sequential(nn.Linear(in_feature,hid),nn.ReLU(),nn.Dropout(0.1))
         self.out = nn.Sequential(nn.Linear(hid,out),nn.Sigmoid())
 
     def forward(self,x):
         x = self.hidden(x)
         x = self.out(x)
-
         return x
 
 #Generator
 class Generator(nn.Module):
-
     def __init__(self):
         super(Generator,self).__init__()
-
         in_feature = 100
         hid = 256
         out = 784
-
         self.hidden = nn.Sequential(nn.Linear(in_feature,hid),nn.ReLU(),nn.Dropout(0.1))
         self.out = nn.Sequential(nn.Linear(hid,out),nn.Tanh())
 
     def forward(self,x):
-
         x = self.hidden(x)
         x = self.out(x)
-
         return x
-
-
+      
 #Creating Model
 generator = Generator().to(device)
 discriminator = Discriminator().to(device)
-
 
 #Adam Optimizer
 generator_optimizer = optim.Adam(generator.parameters(), lr = args.learning_rate, betas=(args.beta1, args.beta2))
@@ -111,69 +97,44 @@ print('-------------------------------------------------------------------')
 print('------------------------Generator----------------------------------\n\n')
 print("Input Shape {}".format((args.batch_size, 100)))
 summary(generator, (args.batch_size, 100))
-
 print('-------------------------------------------------------------------')
 
 print('------------------------Discriminator------------------------------\n\n')
 print("Input Shape {}".format((args.batch_size, 784)))
 summary(discriminator, (args.batch_size, 784))
-
 print('-------------------------------------------------------------------')
-
 
 # Creating latent vector of 100 random Variables
 def random_input(size):
     r = Variable(torch.randn(size,100)).to(device)
     return r
-
+  
 # Traing Generator
 def generator_train(x):
-
     r = random_input(x.shape[0])
-
     fake_label_ones = Variable(torch.ones(x.shape[0])).to(device)
-
     generator_optimizer.zero_grad()
-
     fake_images = generator(r)
-
     fake_predict = discriminator(fake_images)
-
     loss = Loss(fake_predict,fake_label_ones)
-
     loss.backward()
-
     generator_optimizer.step()
-
     return loss
 
 #Training Discriminator
 def discriminator_train(x):
-
     r = random_input(x.shape[0])
-
     discriminator_optimizer.zero_grad()
-
     real_label = Variable(torch.ones(x.shape[0])).to(device)
-
     real_predict = discriminator(x)
-
     real_loss = Loss(real_predict , real_label)
-
     real_loss.backward()
-
     fake_label = Variable(torch.zeros(x.shape[0])).to(device)
-
     fake_predict = discriminator(generator(r).detach())
-
     fake_loss = Loss(fake_predict,fake_label)
-
     fake_loss.backward()
-
     loss = real_loss + fake_loss
-
     discriminator_optimizer.step()
-
     return loss
 
 if 'output' not in os.listdir():
@@ -192,14 +153,12 @@ for epoch in range(args.epochs):
     for batch , (image , label) in enumerate(data):
 
         x = image.view(image.size(0), 784).to(device)
-
         #Training each network simultaneously 
         d_loss = discriminator_train(x)
         g_loss = generator_train(x)
 
         D_loss.append(d_loss)
         G_loss.append(g_loss)
-
 
         if batch%100==0 and args.quiet ==False:
             print("Epoch ",epoch,'/',args.epochs,'Batch ',batch,'/', total_batch,"Generator Loss:",g_loss.item(),'Discriminator Loss:',d_loss.item())
